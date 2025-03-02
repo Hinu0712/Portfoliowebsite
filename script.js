23fea2dd2aa2cec135c4ab108222206fd1444719
Hinu0712 <hinalpatel4121@gmail.com>
2025-03-02 12:44:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ document.getElementById("explore-about-me-btn").addEventListener("click", functi
   document.getElementById("about-me-section").style.display = "block";
   document.getElementById("projects-section").style.display = "none"; // Optional: Hide the other section
 });
+
+fetch('about.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('about-container').innerHTML = data;
+    });
