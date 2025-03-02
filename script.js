6353883b56f0da5fc4650678153bfcfadc5c243c
Hinu0712 <hinalpatel4121@gmail.com>
2025-03-02 10:53:56 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector("form").addEventListener("submit", function(event) {
+        event.preventDefault();
+        alert("Thank you! Your message has been sent.");
+    });
+});
